9a45144af70eb96ea0ec5234892c12b7ed17d5ea
shivam Gupta <shivdevi480@gmail.com>
2025-05-13 10:37:30 +0530

    assigment done

---
+++ task01.py
@@ -0,0 +1,8 @@
+# Input a number
+num = int(input("Enter a number: "))
+
+# Check even or odd using modulus
+if num % 2 == 0:
+    print(num, "is an even number")
+else:
+    print(num, "is an odd number")
---
+++ task02.py
@@ -0,0 +1,4 @@
+total =0
+for i in range(1,50):
+    total +=i
+print("the sum of integer from 1 to 50 is :",total)
